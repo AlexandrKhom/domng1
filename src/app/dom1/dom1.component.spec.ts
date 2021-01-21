edd54b608f7afe36a47a7a25b4c69e2efabf2eff
AlexandrKhom <alex988andr@gmail.com>
2021-01-21 15:06:14 +0200

    1fix1

---
+++ src/app/dom1/dom1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Dom1Component } from './dom1.component';
+
+describe('Dom1Component', () => {
+  let component: Dom1Component;
+  let fixture: ComponentFixture<Dom1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Dom1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Dom1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dom1/dom1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dom1',
+  templateUrl: './dom1.component.html',
+  styleUrls: ['./dom1.component.css']
+})
+export class Dom1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
