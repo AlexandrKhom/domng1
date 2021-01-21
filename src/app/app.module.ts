19310080f34fd2ff6811bee61ad55969499cd461
AlexandrKhom <alex988andr@gmail.com>
2021-01-21 15:05:45 +0200

    1fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Dom1Component } from './dom1/dom1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Dom1Component
   ],
   imports: [
     BrowserModule
